cf20d681f09b82a42d880f4d7208f611d4b4f7cd
Javier Bac <javierbac010@gmail.com>
2026-06-12 04:28:23 -0600

    version inicial de navbar

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,14 @@
+<div class="row w-100">
+    <div class="col-1">
+        <div class="vertical-navbar">
+            <div class="navbar-icons-container mx-auto d-table">
+                <a href="/" class="navbar-icon border-0" title="Javier Juchuña Bac">
+                    <img src="{{ asset('img/webIcon.png') }}" alt="Logo" width="45" class="rounded-circle">
+                </a>
+                <a href="/" class="navbar-icon" title="Inicio"><i class="bi bi-house-door"></i></a>
+                <a href="" class="navbar-icon" title="Proyectos"><i class="bi bi-rocket-takeoff"></i></a>
+                <a href="" class="navbar-icon" title="Trayecto académico"><i class="bi bi-journal-bookmark"></i></a>
+            </div>
+        </div>
+    </div>
+    
